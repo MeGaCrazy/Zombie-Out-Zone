7f2f3b6a5f7adec12c2cb53a7cf1379bfee2beb3
MeGaCrazy <mikaelmonuir@gmail.com>
2018-04-27 14:28:33 +0200

    Player Can Move And perform animation not fully but most

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public float smoothing =5f;
+	Vector3 offset;
+	void Start(){
+		offset = transform.position - target.position;
+	}
+	void FixedUpdate(){	
+		Vector3 targetCampos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCampos, smoothing * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 6f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	float camRayLength = 100f;
+	int floorMask;
+	void Awake(){
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate(){
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+	}
+	void Move(float h,float v){
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+	void Turning ()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+		if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+	void Animating(float h,float v){
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("isWalking", walking);
+	}
+}
