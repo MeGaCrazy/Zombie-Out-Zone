1fe10905a1bd7db7f81e4783632f94e1613544fa
Muhammad khaled <harolinch@gmail.com>
2018-04-28 12:00:32 +0200

    player, zombie, cam update
    added player script to attack and move
    added zombe script simple AI to follow and attack.
    
    added camera to follow player and rotate with mouse

---
+++ Assets/Scripts/Camera/camMouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camMouseLook : MonoBehaviour {
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+    void Start()
+    {
+        character = this.transform.parent.gameObject;    
+    }
+
+    private void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+
+
+}
