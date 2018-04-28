dfd3c9bf32d9f0b1703c1c57be28474fbdefa963
Muhammad khaled <harolinch@gmail.com>
2018-04-28 23:50:02 +0200

    added first zombie

---
+++ Assets/Scripts/zombieScripts/chase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chase : MonoBehaviour {
+
+    public Transform player;
+    static Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+		if(Vector3.Distance(player.position, this.transform.position) < 30 && angle < 180)
+        {
+            
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+
+            anim.SetBool("isIdle", false);
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.05f);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking", false);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isWalking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+        }
+	}
+}
