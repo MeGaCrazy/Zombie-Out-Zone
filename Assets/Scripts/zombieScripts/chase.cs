0de6f7bd7515ce145e1649f6b158219cc988238b
Muhammad khaled <harolinch@gmail.com>
2018-04-29 03:55:52 +0200

    i kill the first zombie

--- Assets/Scripts/zombieScripts/chase.cs
+++ Assets/Scripts/zombieScripts/chase.cs
@@ -5,43 +5,55 @@ using UnityEngine;
 public class chase : MonoBehaviour {
 
     public Transform player;
+    public PlayerHealth ph;
     static Animator anim;
+    public detectHit dethit;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 direction = player.position - this.transform.position;
-        float angle = Vector3.Angle(direction, this.transform.forward);
-		if(Vector3.Distance(player.position, this.transform.position) < 30 && angle < 180)
+        Debug.Log("his health is" + dethit.currentHealth);
+        if (dethit.currentHealth > 0)
         {
-            
-            direction.y = 0;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-
-
-            anim.SetBool("isIdle", false);
-            if(direction.magnitude > 5)
+            Vector3 direction = player.position - this.transform.position;
+            float angle = Vector3.Angle(direction, this.transform.forward);
+            if (Vector3.Distance(player.position, this.transform.position) < 30 && angle < 180)
             {
-                this.transform.Translate(0, 0, 0.05f);
-                anim.SetBool("isWalking", true);
-                anim.SetBool("isAttacking", false);
+
+                direction.y = 0;
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+
+                anim.SetBool("isIdle", false);
+                if (direction.magnitude > 7)
+                {
+                    this.transform.Translate(0, 0, 0.05f);
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isAttacking", false);
+                }
+                else
+                {
+                    anim.SetBool("isAttacking", true);
+                    anim.SetBool("isWalking", false);
+                    ph.TakeDamage(1);
+
+                }
             }
             else
             {
-                anim.SetBool("isAttacking", true);
+                anim.SetBool("isIdle", true);
                 anim.SetBool("isWalking", false);
+                anim.SetBool("isAttacking", false);
             }
         }
         else
         {
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isAttacking", false);
+            anim.SetBool("isDead", true);
         }
 	}
 }
--- Assets/Scripts/zombieScripts/detectHit.cs
+++ Assets/Scripts/zombieScripts/detectHit.cs
@@ -3,20 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class detectHit : MonoBehaviour {
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
 
 
-    void OnTriggerEnter(Collider other)
+    Animator anim;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+    private void Awake()
     {
-        Debug.Log("Hit");
+        anim = GetComponent<Animator>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        currentHealth = startingHealth;
+    }
+    private void Update()
+    {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        if (isDead)
+            return;
+        currentHealth -= amount;
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
+
+        if (currentHealth <= 0)
+        {
+
+            Debug.Log("I came here hahahahahahahahaha");
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        capsuleCollider.isTrigger = true;
+        anim.SetBool("isDead", true);
+
+        Debug.Log("i came here tooooooooo 2");
+    }
+
+
+    public void StartSinking()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+        Destroy(gameObject, 2f);
+    }
+
+
 }
